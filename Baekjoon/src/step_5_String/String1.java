f7bc2e3da2987a729192b76ae2b7d760ef9f9007
mintcoffeee <you6735@naver.com>
2023-01-14 18:41:23 +0900

    문자열

---
+++ Baekjoon/src/step_5_String/String1.java
@@ -0,0 +1,10 @@
+package step_5_String;
+
+import java.util.Scanner;
+
+public class String1 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String a = s.next(); char c = a.charAt(0); System.out.println((int)c);
+	}
+}
---
+++ Baekjoon/src/step_5_String/String2.java
@@ -0,0 +1,11 @@
+package step_5_String;
+
+import java.util.Scanner;
+
+public class String2 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String a = s.next(); System.out.println(a.length());
+	}
+
+}

e763b6c1672e96d58b4d66d0c35b3e6b8b88c592
mintcoffeee <you6735@naver.com>
2023-02-21 16:41:20 +0900

    입출력과 사칙연산

---
+++ Baekjoon/src/step_1/A11.java
@@ -0,0 +1,11 @@
+package step_1;
+
+import java.util.Scanner;
+
+public class A11 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long a=s.nextLong(),b=s.nextLong(), c=s.nextLong();
+		System.out.println(a+b+c);
+	}
+}

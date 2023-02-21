257d29e7882404dfdec01ee38d6d426c71083558
mintcoffeee <you6735@naver.com>
2023-02-21 16:41:34 +0900

    반복문

---
+++ Baekjoon/src/step_3_for/For5.java
@@ -0,0 +1,13 @@
+package step_3_for;
+
+import java.util.Scanner;
+
+public class For5 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		for(int n=s.nextInt()/4, i=0;i<n;i++) {
+			System.out.print("long ");
+		}System.out.println("int");
+	}
+}
